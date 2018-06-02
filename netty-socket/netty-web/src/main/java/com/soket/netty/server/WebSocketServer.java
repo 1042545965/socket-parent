161546790b9b81e8c27c34dc1acc119d527d7e4e
dkzadmin <dkzadmin@DESKTOP-VO7KPPB>
2018-06-02 15:48:02 +0800

    commit

--- netty-socket/netty-web/src/main/java/com/soket/netty/server/WebSocketServer.java
+++ netty-socket/netty-web/src/main/java/com/soket/netty/server/WebSocketServer.java
@@ -156,7 +156,7 @@ public class WebSocketServer extends AfterSpringBegin{
 				protected void initChannel(Channel channel) throws Exception {
 					ChannelPipeline pipeline = channel.pipeline();
 					pipeline.addLast("http-codec", new HttpServerCodec()); // Http消息编码解码
-					pipeline.addLast("aggregator", new HttpObjectAggregator(65536)); // Http消息组装
+					pipeline.addLast("aggregator", new HttpObjectAggregator(65536)); // Http消息组装大小
 					pipeline.addLast("http-chunked", new ChunkedWriteHandler()); // WebSocket通信支持
 					pipeline.addLast("handler", new BananaWebSocketServerHandler()); // WebSocket服务端Handler
 				}
